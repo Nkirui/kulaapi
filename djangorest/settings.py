f715bf6b5653e52eb5b4268b16822cf5e4cefdf0
Nkirui <nathankirui5@gmail.com>
2017-06-27 18:02:17 +0300

    Added files and changes required for deployment to heroku

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -133,3 +133,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
